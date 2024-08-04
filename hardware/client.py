1cc59d2fa6cdf637df7dcfa63edc1feafb9d4e65
Ye0ngjae <op@ye0ngjae.com>
2024-08-05 02:54:38 +0900

    test

--- hardware/client.py
+++ hardware/client.py
@@ -22,7 +22,7 @@ pwm = GPIO.PWM(MOTOR_PIN, 1000)  # 1000 Hz frequency
 pwm.start(0)  # Start with 0% duty cycle
 
 # Create a camera streamer
-streamer = PiCameraStreamer(server_url=URL)
+streamer = PiCameraStreamer(server_url=URL+"train/video/1")
 
 # Start the camera streamer
 streamer.start()
