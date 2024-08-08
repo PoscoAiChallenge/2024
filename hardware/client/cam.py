75c774e17018f38105e57f7f3a5acdd24b250e3e
Ye0ngjae <op@ye0ngjae.com>
2024-08-08 13:12:00 +0900

    fix cam

--- hardware/client/cam.py
+++ hardware/client/cam.py
@@ -6,13 +6,8 @@ camera = Picamera2()
 camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
 camera.start()
 
-def generate_frames():
-    while True:
+while True:
         frame = camera.capture_array()
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         requests.post('http://192.168.124.101:5000/post_frame', files={'frame': frame}, timeout=0.1)
-
-generate_frames()
