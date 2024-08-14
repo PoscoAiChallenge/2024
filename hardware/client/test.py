72269a7680e541c478508a0ba277833baa1ecc78
Ye0ngjae <op@ye0ngjae.com>
2024-08-15 00:14:19 +0900

    edit socket

--- hardware/client/test.py
+++ hardware/client/test.py
@@ -1,14 +1,24 @@
-import socket
+
 import json
 import base64
 from dotenv import load_dotenv
 import requests
 import os
+import gpiozero
 
 load_dotenv()
 URL = os.getenv('URL')
 NUM_TRAIN = os.getenv('TRAIN')
 
+motor = gpiozero.LED(18)
+
 while True:
     res = requests.get(URL + '/speed/' + NUM_TRAIN)
-    print(res.status_code)
+    number = res.json().get('status')
+    if number == '0':
+        motor.off()
+    elif number == '1':
+        motor.on()
+    else:
+        print('Invalid speed value:', number)
+        motor.off()
