708101ac79015c562b59959d8f5fb04127279174
Ye0ngjae <op@ye0ngjae.com>
2024-08-17 21:00:10 +0900

    edit client

--- hardware/client/test.py
+++ hardware/client/test.py
@@ -61,8 +61,8 @@ while True:
     time.sleep(2)
 
     server.sendall(image_length.encode().ljust(64))
-    server.send(base64_image.encode())
-    server.send(stime.encode().ljust(64))
+    #server.send(base64_image.encode())
+    #server.send(stime.encode().ljust(64))
 
     data = json.dumps({
         "train_id": str(NUM_TRAIN),
