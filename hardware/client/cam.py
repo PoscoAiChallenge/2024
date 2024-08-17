888dc5feb8d879bf9989d735053dd530b849662c
Ye0ngjae <op@ye0ngjae.com>
2024-08-17 19:13:59 +0900

    edit client

--- hardware/client/cam.py
+++ hardware/client/cam.py
@@ -63,6 +63,7 @@ def generate_frames():
             continue
 
         frame_bytes = buffer.tobytes()
+        print()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
--- hardware/client/test.py
+++ hardware/client/test.py
@@ -20,16 +20,48 @@ camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888',
 camera.start()
 
 def generate_frames():
-    while True:
-        frame = camera.capture_array()
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+    prev_frame = None
+    motion_threshold = 5000  # 조정 가능한 임계값
 
-        return frame
-    
+    while True:
+        if frame_buffer.empty():
+            time.sleep(0.001)
+            continue
+
+        frame = frame_buffer.get()
+        
+        # YUV420에서 BGR로 변환
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_I420)
+
+        # 모션 감지 (필요한 경우)
+        if prev_frame is not None:
+            diff = cv2.absdiff(cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY),
+                               cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY))
+            non_zero = cv2.countNonZero(diff)
+            if non_zero <= motion_threshold:
+                prev_frame = frame_bgr
+                continue  # 모션이 감지되지 않으면 다음 프레임으로
+
+        prev_frame = frame_bgr
+
+        # JPEG 압축
+        ret, buffer = cv2.imencode('.jpg', frame_bgr, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        if not ret:
+            continue
+
+        frame_bytes = buffer.tobytes()
+        
+        return frame_bytes
+
 while True:
     image = generate_frames()
     base64_image = base64.b64encode(image).decode('utf-8')
+    image_length = str(len(base64_image))
+    stime = datetime.utnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    server.sendall(image_length.encode())
+    server.send(base64_image.encode())
+    server.send(stime.encode())
 
     data = f'''
     {{
